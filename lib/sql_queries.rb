6d7bdf9a06a8f700e3923336804424c3818258bc
Kamrin <kamrinkennedy@gmail.com>
2020-06-01 13:35:26 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,10 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(color, count(*)) FROM bears"
+  "SELECT COUNT(*) as Result FROM blog_posts
+  GROUP BY blog_id
+  ORDER BY Result DESC
+  LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
